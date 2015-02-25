098d4900c2aaaf2e313dc37e8d60e7645aa74b1a
mrshelly <mrshellymrshelly@hotmail.com>
2015-02-26 03:49:41 +0800

    换个方式备份数据库

--- openerp/addons/auto_backup/backup_scheduler.py
+++ openerp/addons/auto_backup/backup_scheduler.py
@@ -3,7 +3,7 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
-#    Copyright (C) 2012-2013 Mrshelly@gmail.com  upgrade to 7.0
+#    Copyright (C) 2012-2015 Mrshelly@gmail.com  upgrade to 7.0
 #    Copyright (C) 2014      JianJian@osbzr.com  upgrade to 8.0
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -21,21 +21,20 @@
 #
 ##############################################################################
 
-from openerp.osv import fields,osv
+from openerp.osv import fields, osv
 import xmlrpclib
 import socket
 import os
 import time
 import base64
+import logging
 
 import pytz
 import datetime
-import logging
 
 from openerp import tools
 from openerp import netsvc
 
-#logger = netsvc.Logger()
 _logger = logging.getLogger(__name__)
 
 def execute(connector, method, *args):
@@ -114,7 +113,6 @@ class db_backup(osv.osv):
                     bkp = execute(conn, 'dump', master_pass, rec.name)
                 except:
                     _logger.info('backup',"Could'nt backup database %s. Bad database administrator password for server running at http://%s:%s" %(rec.name, rec.host, rec.port))
-                    #logger.notifyChannel('backup', netsvc.LOG_INFO, "Could'nt backup database %s. Bad database administrator password for server running at http://%s:%s" %(rec.name, rec.host, rec.port))
                     continue
                 bkp = base64.decodestring(bkp)
                 fp = open(file_path,'wb')
@@ -122,9 +120,70 @@ class db_backup(osv.osv):
                 fp.close()
             else:
                 _logger.info('backup', "database %s doesn't exist on http://%s:%s" %(rec.name, rec.host, rec.port))
-                #logger.notifyChannel('backup', netsvc.LOG_INFO, "database %s doesn't exist on http://%s:%s" %(rec.name, rec.host, rec.port))
 
         return True
+
+    def schedule_backup_pgtool(self, cr, uid, ids, context=None):
+        context = context or {}
+        conf_ids= self.search(cr, uid, [])
+        confs = self.browse(cr,uid,conf_ids)
+        master_pass = tools.config.get('admin_passwd', False)
+        res_user_obj = self.pool.get('res.users')
+        if not master_pass:
+            raise
+        for rec in confs:
+            db_list = self.get_db_list(cr, uid, [], rec.host, rec.port)
+            # Get UTC time
+            curtime = datetime.datetime.strptime(datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
+            res_user_res = res_user_obj.browse(cr, uid, uid, context=context)
+            # user's partner timezone
+            tz = pytz.timezone(res_user_res.tz) if res_user_res.tz else pytz.utc
+            # Set to usre's localtime
+            curtime = pytz.utc.localize(curtime).astimezone(tz)
+            #curtime = curtime.astimezone(pytz.utc)
+
+            if rec.name in db_list:
+                try:
+                    if not os.path.isdir(rec.bkp_dir):
+                        os.makedirs(rec.bkp_dir)
+                except:
+                    raise
+                bkp_file='%s_%s.sql' % (rec.name, curtime.strftime('%Y%m%d_%H_%M_%S'))
+                file_path = os.path.join(rec.bkp_dir,bkp_file)
+                bkp=''
+                try:
+                    self._db_pg_dump(cr, uid, rec.name, file_path, context=context)
+                except Exception,ex:
+                    _logger.warn('auto_backup DUMP DB except: '+str(ex))
+                    continue
+        return True
+
+    def _db_pg_dump(self, cr, uid, db_name, db_filename, context=None):
+        _logger.info('auto_backup DUMP DB!')
+        pg_passwd = os.environ.get('PGPASSWORD') or tools.config['db_password'] or False
+        if not pg_passwd:
+            _logger.error(
+                    'DUMP DB: %s failed! Please verify the configuration of the database password on the server. '
+                    'You may need to create a .pgpass file for authentication, or specify `db_password` in the '
+                    'server configuration file.\n %s', db_name, data)
+            raise Exception, "Couldn't dump database"
+        cmd = ['pg_dump', '--format=c', '--no-owner']
+        if tools.config['db_user']:
+            cmd.append('--username=' + tools.config['db_user'])
+        if tools.config['db_host']:
+            cmd.append('--host=' + tools.config['db_host'])
+        if tools.config['db_port']:
+            cmd.append('--port=' + str(tools.config['db_port']))
+        cmd.append('--file=' + db_filename)
+        cmd.append(db_name)
+
+        stdin, stdout = tools.exec_pg_command_pipe(*tuple(cmd))
+        stdin.close()
+        data = stdout.read()
+        res = stdout.close()
+
+        return base64.encodestring(data)
+
 
 db_backup()
 
