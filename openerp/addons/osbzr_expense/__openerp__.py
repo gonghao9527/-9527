5e0e6f8218e585a3b575debefc1aca708ac812b6
Jacky_Song <jacky@osbzr.com>
2015-09-06 10:08:15 +0800

    [fix]人力资源费用增加直接付款方式修改

--- openerp/addons/osbzr_expense/__openerp__.py
+++ openerp/addons/osbzr_expense/__openerp__.py
@@ -4,13 +4,14 @@
     "name": "报销单上直接选择付款方式",
     "description":
         """
-                                人力资源费用报销单上直接选择付款方式
+            原有功能是员工提交报销单，经会计过账后生成会计凭证，记账后针对员工付款，
+            此模块增加直接选择付款方式,满足客户报销直接付款不挂账业务模式，生成会计凭证。
         """,
     'author': "jacky@osbzr.com,jason@osbzr.com",
-    'website': "http://",
+    'website': "http://www.osbzr.com",
 
     "category": "osbzr",
-    "version": "8.0.0.1",
+    "version": "1.0",
     "depends": [
                 'hr_expense','base'
                 ],
