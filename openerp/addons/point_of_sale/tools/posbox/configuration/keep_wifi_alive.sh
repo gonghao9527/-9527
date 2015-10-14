4d8cf2b9b6e7bd218410589797b7675c7a781c6d
Jacky_Song <jacky@osbzr.com>
2015-10-14 15:27:28 +0800

    [IMP] point_of_sale: posbox添加远程调试功能

--- openerp/addons/point_of_sale/tools/posbox/configuration/keep_wifi_alive.sh
+++ openerp/addons/point_of_sale/tools/posbox/configuration/keep_wifi_alive.sh
@@ -6,7 +6,7 @@ while true ; do
 		ESSID=$(head -n 1 "${CURRENT_WIFI_NETWORK_FILE}" | tr -d '\n')
 		PASSWORD=$(tail -n 1 "${CURRENT_WIFI_NETWORK_FILE}" | tr -d '\n')
 		logger -t posbox_keep_wifi_alive "Lost wifi, trying to reconnect to ${ESSID}"
-		sudo /home/pi/odoo/addons/point_of_sale/tools/posbox/configuration/connect_to_wifi.sh "${ESSID}" "${PASSWORD}"
+		sudo /home/pi/odoo/addons/point_of_sale/tools/posbox/configuration/connect_to_wifi.sh "${ESSID}" "${PASSWORD}" "" "NO_AP"
 		sleep 30
 	fi
 	sleep 2
